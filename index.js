5ba3cf6134a1dc393cf17a686d33e0c90d618e07
Ambs04 <amberleejones2007@gmail.com>
2024-11-23 14:47:14 +0200

    get element for order total and create list element for orders

--- index.js
+++ index.js
@@ -42,7 +42,9 @@ function displayMenuItems(menu) {
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
   // Get the order items list and the order total element from the HTML
+  let orderList = document.getElementById("order-total");
   // Create a list item for the order
+  let orderListing = document.createElement("li");
   // Set the text content of the list item to the item name
   // Append the list item to the order items list
   // Calculate and update the total price
